5cdc12fa971c4872b7c0de6a81be7b641874220c
Vinay Kumar Gupta <vinay.kumar.gupta@sap.com>
2025-06-06 13:22:11 +0530

    Fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders analytical table', () => {
   render(<App />);
   const tableElement = screen.getByText("Name");
   screen.debug(tableElement);
